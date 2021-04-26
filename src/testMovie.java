183c55fad26ed1745810a5316fbec07db94ca563
Benjamin Hendrix <bih1@students.uwf.edu>
2021-04-26 13:31:32 -0500

    Pulled in MovieDatabase branch

--- src/testMovie.java
+++ src/testMovie.java
@@ -1,3 +1,4 @@
+import static junit.framework.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -16,7 +17,7 @@ class testMovie {
 		assertEquals(0, tm.getNumReviews());
 		assertEquals(0.0, tm.getRating(), 0.001);
 	}
-	
+
 	@Test
 	void testNameConstructor() {
 		Movie tm1 = new Movie("Real Movie");
@@ -29,7 +30,7 @@ class testMovie {
 		assertEquals(0, tm1.getNumReviews());
 		assertEquals(0.0, tm1.getRating(), 0.001);
 	}
-	
+
 	@Test
 	void testReviewConstructor() {
 		Movie tm2 = new Movie("Real Movie", 20, 10, 50);
@@ -41,45 +42,57 @@ class testMovie {
 		}
 		assertEquals(10, tm2.getNumReviews());
 		assertEquals(100, tm2.getRating(), 0.001);
-		
+
 		Movie tm3 = new Movie("AlsoReal Movie", 20, 10, 0);
 		assertEquals(0.0, tm3.getRating(), 0.001);
-		
+
 		Movie tm4 = new Movie ("Unreal Movie", 20, 10, 25);
 		assertEquals(50.0, tm4.getRating(), 0.001);
-		
+
 		Movie tm5 = new Movie ("Maybe Real Movie?", 20, 10, 11);
 		assertEquals(22.0, tm5.getRating(), 0.001);
 	}
-	
+
 	@Test
 	void testTagsConstructor() {
 		String[] t = {"Good", "Not Bad", "Not Great", "Old", "Ew"};
 		Movie tm6 = new Movie("Real Movie", 20, 10, 50, 5, t);
 		assertEquals("Real Movie", tm6.getName());
 		assertEquals(20, tm6.getViews());
-		assertEquals(0, tm6.getNumTags());
+		assertEquals(5, tm6.getNumTags());
 		for (int i = 0; i < 5; i++) {
 			assertEquals(t[i], tm6.getTags()[i]);
 		}
 		assertEquals(10, tm6.getNumReviews());
 		assertEquals(100, tm6.getRating(), 0.001);
-		
+
 		String[] t1 = {"Good", "Not Bad", "Not Great"};
 		Movie tm7 = new Movie("Also Real Movie", 20, 10, 50, 3, t1);
-		assertEquals(0, tm7.getNumTags());
+		assertEquals(3, tm7.getNumTags());
 		for (int i = 0; i < 3; i++) {
 			assertEquals(t1[i], tm7.getTags()[i]);
 		}
 		for (int i = 3; i < 5; i++) {
 			assertEquals("", tm7.getTags()[i]);
 		}
-		
+
 		String[] t2 = {};
 		Movie tm8 = new Movie("Unreal Movie", 20, 10, 50, 0, t2);
 		for (int i = 0; i < 5; i++) {
 			assertEquals("", tm8.getTags()[i]);
 		}
+	}
+
+	@Test
+	void testRatingConstructor() {
+		String[] t = {"Sci-Fi", "Action", "Adventure", "Drama"};
+		Movie tm9 = new Movie("Star Wars: Episode IV - A New Hope", 1000, 500, 92.0, 4, t);
+		assertEquals("Star Wars: Episode IV - A New Hope", tm9.getName());
+		assertEquals(1000, tm9.getViews());
+		assertEquals(500, tm9.getNumReviews());
+		assertEquals(92, tm9.getRating());
+		assertEquals(4, tm9.getNumTags());
+		assertEquals("Sci-Fi", tm9.getTags()[0]);
 	}
 
 }
