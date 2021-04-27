e37e0f01d56bb37b6ed555b00f63fca601a0c02e
Benjamin Hendrix <bih1@students.uwf.edu>
2021-04-27 09:16:35 -0500

    Added ItemToItem collaborative filtering

--- src/CorrelationAgent.java
+++ src/CorrelationAgent.java
@@ -17,10 +17,8 @@ public class CorrelationAgent {
 	/**
 	 * @implNote Sets up the users list with a default amount
 	 */
-	private void InitializeUsers()
-	{
-		for(int i=0; i < DEFAULT_NUM_USERS; i++)
-		{
+	private void InitializeUsers() {
+		for(int i=0; i < DEFAULT_NUM_USERS; i++) {
 			this.users.add(new User());
 		}
 	}
@@ -29,8 +27,7 @@ public class CorrelationAgent {
 	 * 
 	 * @param users
 	 */
-	public CorrelationAgent(ArrayList<User> users) throws FileNotFoundException
-	{
+	public CorrelationAgent(ArrayList<User> users) throws FileNotFoundException {
 		this.users = users;
 	}
 	
@@ -39,38 +36,78 @@ public class CorrelationAgent {
 	 * @param user
 	 * @return
 	 */
-	public String RecommendMovieToUser(User user, int locOfUserInUsersArray)
-	{
-		User bestMatchingUser = new User("Test user");
-		double max = 0;
-		double temp;
-		
-		for(int i=0; i<2; i++)
-		{
-			if(i != locOfUserInUsersArray)
-			{
-				temp = this.CenteredCosineSimilarityBetweenTwoUsers(user, users.get(i));
-				if( max < temp)
-				{
-					max = temp;
-					bestMatchingUser = users.get(i);
-				}
-			}
-		}
-		bestMatchingUser.CalculateAvgRatingsMinusMean();
-
-		double highestScore = 0.0;
-		int loc = 0;
-		for(int i=0; i<User.DEFAULT_NUM_MOVIES; i++) {
-			if (user.GetHasRatedMovie()[i] == false) {
-				if (highestScore < bestMatchingUser.GetAvgRatingsMinusMean()[i]) {
-					highestScore = bestMatchingUser.GetAvgRatingsMinusMean()[i];
-					loc = i;
-				}
-			}
+	public String RecommendMovieToUser(User user, int locOfUserInUsersArray) {
+		User bestMatch = FindBestMatchingUser(user, locOfUserInUsersArray);
+		int index = UserToUserCollaborativeFiltering(user, bestMatch);
+		int maxScore = (int)bestMatch.GetAvgRatingsMinusMean()[index];
+		if(maxScore <= 0) {
+			index = ItemToItemCollaborativeFiltering(user);
 		}
 		 
-		return movies.getMovies().get(loc).getName();
+		return movies.getMovies().get(index).getName();
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int ItemToItemCollaborativeFiltering(User user) {
+		int max = Integer.MIN_VALUE;
+		int index = 0;
+		for(int i=0; i < movies.getMovies().size(); i++) {
+			if(!user.GetHasRatedMovie()[i] && max < (int)movies.getMovies().get(i).getRating()) {
+				max = (int)movies.getMovies().get(i).getRating();
+				index = i;
+				System.out.println(""+movies.getMovies().get(i).getName()+" ");
+			}
+		}
+		
+		return index;
+	}
+	
+	/**
+	 * 
+	 * @param user
+	 * @param bestMatch
+	 * @return
+	 */
+	public int UserToUserCollaborativeFiltering(User user, User bestMatch) {
+		double max = Integer.MIN_VALUE;
+		int index = 0;
+		for(int i=0; i < movies.getMovies().size(); i++) {
+			if(user.GetHasRatedMovie()[i] == false) {
+				if(max < bestMatch.GetAvgRatingsMinusMean()[i]) {
+					max = bestMatch.GetAvgRatingsMinusMean()[i];
+					index = i;
+				}
+			}
+		}
+		
+		return index;
+	}
+
+	/**
+	 * 
+	 * @param user
+	 * @param locOfUserInUsersArray
+	 * @return
+	 */
+	public User FindBestMatchingUser(User user, int indexOfUser) {
+		double num = 0.0;
+		double max = Integer.MIN_VALUE;
+		User bestMatch = new User();
+		for (int i = 0; i < users.size (); i++) {
+			if(i != indexOfUser) {
+				num = this.CenteredCosineSimilarityBetweenTwoUsers(user, users.get(i));
+				if(max < num) {
+					max = num;
+					bestMatch = users.get(i);
+				}
+			}
+		}
+		bestMatch.CalculateAvgRatingsMinusMean();
+		
+		return bestMatch;
 	}
 	
 	/**
@@ -79,11 +116,10 @@ public class CorrelationAgent {
 	 * @param userB
 	 * @return
 	 */
-	public double CenteredCosineSimilarityBetweenTwoUsers(User userA, User userB)
-	{
+	public double CenteredCosineSimilarityBetweenTwoUsers(User userA, User userB) {
 		double dotProductXY = CalculateVectorDotProduct(userA, userB);
-		double magnitudeX = this.CalculateMagnitudeOfVector(userA);
-		double magnitudeY = this.CalculateMagnitudeOfVector(userB);
+		double magnitudeX = CalculateMagnitudeOfVector(userA);
+		double magnitudeY = CalculateMagnitudeOfVector(userB);
 		double crossProductXY = magnitudeX * magnitudeY;
 		
 		return dotProductXY / crossProductXY;
@@ -95,12 +131,9 @@ public class CorrelationAgent {
 	 * @param userB
 	 * @return
 	 */
-	public double CalculateVectorDotProduct(User userA, User userB)
-	{
+	public double CalculateVectorDotProduct(User userA, User userB) {
 		double dotProduct = 0.0;
-		
-		for(int i=0; i < User.DEFAULT_NUM_MOVIES; i++)
-		{
+		for(int i=0; i < User.DEFAULT_NUM_MOVIES; i++) {
 			dotProduct += (userA.GetAvgRatingsMinusMean()[i] * userB.GetAvgRatingsMinusMean()[i]);
 		}
 		
@@ -112,15 +145,11 @@ public class CorrelationAgent {
 	 * @param user
 	 * @return the magnitude or length of the vector
 	 */
-	public double CalculateMagnitudeOfVector(User user)
-	{
+	public double CalculateMagnitudeOfVector(User user) {
 		double magnitude = 0.0;
-		
-		for(int i=0; i < User.DEFAULT_NUM_MOVIES; i++)
-		{
+		for(int i=0; i < User.DEFAULT_NUM_MOVIES; i++) {
 			magnitude += (user.GetAvgRatingsMinusMean()[i] * user.GetAvgRatingsMinusMean()[i]);
 		}
-		
 		magnitude = Math.sqrt(magnitude);
 		
 		return magnitude;
