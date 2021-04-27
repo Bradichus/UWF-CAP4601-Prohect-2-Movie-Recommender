afb57b2f4d9d0ce17e30b07985db5d420ddce29a
Benjamin Hendrix <bih1@students.uwf.edu>
2021-04-27 15:53:58 -0500

    initial add

--- src/Menu.java
+++ src/Menu.java
@@ -1,10 +1,87 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Menu {
-	Menu() {
-		
+	ArrayList<MovieDatabase> movies;
+	ArrayList<UserDatabase> users;
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		Menu myRecommender = new Menu();
 	}
 	
-	static void main(String args[]) {
+	/**
+	 * 
+	 */
+	public Menu() {
+		this.movies = new ArrayList<MovieDatabase>();
+		this.users = new ArrayList<UserDatabase>();
+		MainMenu();
+	}
+	
+	/**
+	 * 
+	 */
+	public void MainMenu() {
+		Scanner scnr = new Scanner(System.in);
+		boolean quit = false;
+		int option;
+		System.out.println("  -=| MOVIE RECOMMENDER |=-  ");
+		System.out.println("");
 		
+		do {
+			do {
+				MenuOptions();
+				option = scnr.nextInt();
+			} while(!IsValidOption(option));
+			
+			switch(option) {
+			case 1:
+				//Print Moive Database
+				break;
+			case 2:
+				//Print User Database
+				break;
+			case 3:
+				//Add a user
+				break;
+			case 4:
+				//Recommend a movie for user
+				break;
+			case 5:
+				quit = true;
+			}
+		} while(!quit);
+		
+		scnr.close();
+	}
+	
+	/**
+	 * 
+	 */
+	public void MenuOptions() {
+		System.out.println("1) View the Movie Database");
+		System.out.println("2) View the User Database");
+		System.out.println("3) Add a new user to the User Database");
+		System.out.println("4) Ask for a user's next recommended movie");
+		System.out.println("5) Quit");
+		System.out.println("");
+		System.out.print("Enter an option number to proceed: ");
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean IsValidOption(int x) {
+		if(0 < x && x < 6) {
+			return true;
+		}
+		System.out.println("~> Enter a valid number from the list");
+		return false;
 	}
 }
--- src/UserDatabase.java
+++ src/UserDatabase.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserDatabase {
-	
 	ArrayList<User> Users = new ArrayList<User>();
 	
 	UserDatabase() throws FileNotFoundException {
